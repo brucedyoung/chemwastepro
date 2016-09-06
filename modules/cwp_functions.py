01b3232363c0ea92aa493a721d4526e0b518100f
Bruce Young <brucedyoung@gmail.com>
2016-09-06 12:36:40 -0700

    Abstracted cwp_functions, added shelve button

--- modules/cwp_functions.py
+++ modules/cwp_functions.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from gluon import *
+from gluon import current
+
+#cwp_functions
 
 def shelveitem(item_id):
     # This process processes items with blank container or shelf number
@@ -19,3 +22,24 @@ def shelveitem(item_id):
     # Cabinets and shelves have to be assigned manually, not programmatically. They should correspond to physical layout reality.
     response = "shelf number"
     return(response)
+def returndisposalcode(chemindex_id):
+    db = current.db
+    chemhazardlistc = "" 
+    chemindex_row = db(db.chemindex.id == int(chemindex_id)).select().first() #should be only one
+    if ( chemindex_row):#returned a row
+        #find hazards
+        rows = db(db.chemindex_hazard_association.chemindex_id == chemindex_row.id).select(orderby=db.chemindex_hazard_association.hazard_order)
+        for row in rows:
+            chemhazardlistc += row.hazard_id.hazardabbrev+","
+        #remove trailing comma
+        chemhazardlist = chemhazardlistc[:-1]
+        group = "" if chemindex_row.group_ is None else chemindex_row.group_[:2].upper()
+        state = "" if chemindex_row.state is None else chemindex_row.state[:1]
+        #chemhazardlist
+        tsdf = "" if chemindex_row.tsdf is None else chemindex_row.tsdf.tsdfname.upper()
+        treatment = "" if chemindex_row.treatment is None else chemindex_row.treatment.treatnameabbrev.upper()
+        if((group>"")or(state>"")or(chemhazardlist>"")or(tsdf<"")or(treatment>"")):
+            disposalcode=group+"-"+state+"-"+chemhazardlist+"-"+tsdf+"-"+treatment
+        else:
+            disposalcode = ""
+    return (disposalcode)
